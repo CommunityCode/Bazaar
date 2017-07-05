f59b1f599673917059d76c13a52b63b0886e2d85
Elan Moyal <elanmoyal@outlook.com>
2017-07-05 16:21:34 -0400

    Model setup

---
+++ bazaar/bazaar/bazaar/Model/Ingredient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bazaar.Model
+{
+    class Ingredient
+    {
+        public String Name { get; set; }
+        public DateTime Bought { get; set; }
+        public String PlaceBought { get; set; }
+        public String AddressPlaceBought { get; set; }
+        public bool Have { get; set; }
+        public TimeSpan TimeTillFinished { get; set; }
+    }
+}
---
+++ bazaar/bazaar/bazaar/Model/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bazaar.Model
+{
+    class User
+    {
+        public String Name { get; set; }
+        public List<Ingredient> Ingredients { get; }
+    }
+}
